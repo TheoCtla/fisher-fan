dcff3a65de91f2603abf512a16e219764fcd907e
CharlesDesc06 <charles.deschamps06@hotmail.com>
2025-11-21 15:04:30 +0100

    add go mod

---
+++ back/src/api/handler/boat.go
@@ -0,0 +1,9 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func getBoatsHandler(c *gin.Context) {
+	// Implementation for getting all boats
+}
---
+++ back/src/api/model/boat.go
@@ -0,0 +1,8 @@
+package model
+
+type Boat struct {
+	ID     uint    `json:"id" gorm:"primaryKey"`
+	Name   string  `json:"name"`
+	Type   string  `json:"type"`
+	Length float64 `json:"length"`
+}
